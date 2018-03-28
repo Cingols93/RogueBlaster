e313ba56c837eedfc2f6de6b363868e89126ac44
nameless24 <stefano92fred@gmail.com>
2018-03-28 15:42:36 +0200

    sdf

--- src/main/java/it/consoft/rogueblaster/util/Constant.java
+++ src/main/java/it/consoft/rogueblaster/util/Constant.java
@@ -2,14 +2,14 @@ package it.consoft.rogueblaster.util;
 
 public final class Constant {
 	
-	public final static int MAP_WIDTH_SMALL = 10;
-	public final static int MAP_HEIGHT_SMALL = 5;
+	public final static int MAP_WIDTH_SMALL = 12;
+	public final static int MAP_HEIGHT_SMALL = 6;
 	
-	public final static int MAP_WIDTH_MEDIUM = 20;
-	public final static int MAP_HEIGHT_MEDIUM = 10;
+	public final static int MAP_WIDTH_MEDIUM = 12;
+	public final static int MAP_HEIGHT_MEDIUM = 6;
 	
-	public final static int MAP_WIDTH_BIG = 30;
-	public final static int MAP_HEIGHT_BIG = 15;
+	public final static int MAP_WIDTH_BIG = 12;
+	public final static int MAP_HEIGHT_BIG = 6;
 	
 	public final static int MAX_ENEMY_SPAWN_SMALL = 2;
 	public final static int MAX_ENEMY_SPAWN_MEDIUM = 4;	
@@ -26,7 +26,7 @@ public final class Constant {
 	
 	public final static int ROGUE_STR = 5;
 	public final static int ROGUE_AGI = 9;
-	public final static int ROGUE_VIT = 3;
+	public final static int ROGUE_VIT = 5;
 	public final static int ROGUE_LCK = 9;
 	
 
